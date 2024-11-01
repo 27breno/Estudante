1ba848afea35c3757eba6cbfb1a865ef7a7637bf
27breno <brenoferreira1327@gmail.com>
2024-11-01 16:33:42 -0300

    01/11
    Criando InicializadorBD

---
+++ Estudante/Repository/Data/InicializadorBD.cs
@@ -0,0 +1,36 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Estudante.Repository.Data
+{
+    public static class InicializadorBD
+    {
+        private const string ConnectionString = "Data Source=Estudate.db";
+
+
+        public static void Inicializar()
+        {
+            using (var connection = new SQLiteConnection(ConnectionString))
+            {
+                connection.Open();
+                string commandoSQL = @"   
+                 CREATE TABLE IF NOT EXISTS Estudantes(
+                 Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                 Altura REAL NOT NULL,
+                 Peso REAL NOT NULL,
+                 Idade INT NOT NULL,
+                 Nome TEXT NOT NULL
+                );";
+
+            
+
+               connection.Execute(commandoSQL);
+            }
+        }
+    }
+}
---
+++ Estudante/Repository/EstudanteRepository.cs
@@ -0,0 +1,17 @@
+﻿using Dapper.Contrib.Extensions;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Estudante.Repository.EstudanteRepository;
+
+namespace Estudante.Repository
+{
+    public class EstudanteRepository
+    {
+
+    }
+}
