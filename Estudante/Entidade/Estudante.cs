b9d13eac3c5d5e699362feba6c1aca71ceac0b37
27breno <brenoferreira1327@gmail.com>
2024-11-01 16:13:01 -0300

    Adicionar arquivos de projeto.

---
+++ Estudante/Entidade/Estudante.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.PortableExecutable;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Estudante.Entidade
+{
+    public class Estudante
+    {
+        public int Id { get; set; }
+        public double Altura { get; set; }
+        public double Peso { get; set;}
+        public int Idade { get; set;}
+        public string Nome { get; set;}
+    }
+}
